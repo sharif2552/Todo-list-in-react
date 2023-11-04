adc164868a3a56377a4c1eef1a2da8b8305d756d
sharif2552 <86832372+sharif2552@users.noreply.github.com>
2023-11-05 00:20:13 +0600

    removed redux

--- src/components/add.jsx
+++ src/components/add.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addTodo, deleteTodo } from '../store/todoSlice'; 
+
 function AddTodos() {
-  const [task, setTask] = useState('');
-  const dispatch = useDispatch();
-  const todos = useSelector((state) => state.todos); // Retrieve the todos from the Redux store
+  const [task, setTask] = useState(''); // State for the current input value
+  const [todos, setTodos] = useState([]); // State for the list of todos
 
   const handleAddTodo = () => {
     if (task.trim() !== '') {
-      dispatch(addTodo(task)); // Dispatch the addTodo action
-      setTask('');
+      setTodos([...todos, task]); // Adds the new task to the todos array
+      setTask(''); // Resets the task input to be empty after adding to the list
     }
   }
 
   const handleDeleteTodo = (index) => {
-    dispatch(deleteTodo(index)); // Dispatch the deleteTodo action with the index
+    const newTodos = todos.filter((_, i) => i !== index); // Creates a new array without the deleted task
+    setTodos(newTodos); // Sets the new todos array as the current state
   }
 
   return (
@@ -26,25 +25,25 @@ function AddTodos() {
           className="form-control"
           placeholder="Enter a task"
           value={task}
-          onChange={(e) => setTask(e.target.value)} // Update the task state
+          onChange={(e) => setTask(e.target.value)} // Updates the task state as the input changes
         />
         <div className="input-group-append">
           <button
             className="btn btn-primary"
             type="button"
-            onClick={handleAddTodo}
+            onClick={handleAddTodo} // Calls handleAddTodo when the button is clicked
           >
             Add
           </button>
         </div>
       </div>
       <ul className="list-group">
-        {todos.map((todo, index) => (
+        {todos.map((todo, index) => ( // Maps over the todos array and renders each todo as a list item
           <li className="list-group-item d-flex justify-content-between" key={index}>
             {todo}
             <button
               className="btn btn-danger btn-sm"
-              onClick={() => handleDeleteTodo(index)}
+              onClick={() => handleDeleteTodo(index)} // Calls handleDeleteTodo with the index of the todo to be deleted
             >
               Delete
             </button>
